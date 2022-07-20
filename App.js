eb7bd70311a235e04488f9ee912d20306454d3a8
Amal-da <amal.daoud@esprit.tn>
2022-07-20 13:30:06 +0100

    Navigation

--- App.js
+++ App.js
@@ -1,28 +1,26 @@
-import React  from 'react'; 
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Caamera from './screens/Sendphoto';
-import Home from './screens/home';
-import Photo from './screens/photo';
-import Chat from './screens/chat';
-import Pick from './screens/PickPhoto'
+import MyTabs from './screens/tab';
+import Reaction from './screens/react';
+import { View } from 'react-native';
+
 
 const Stack = createStackNavigator();
 
 
 export default function App() {
   return (
+
     
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} options={{ title: 'Home' }} />
-       <Stack.Screen name="Photo" component={Photo}  />
-       <Stack.Screen name="Caamera" component={Caamera}  />
-       <Stack.Screen name="Chat" component={Chat}  />
-       <Stack.Screen name="Pick" component={Pick}  />
-      </Stack.Navigator>
+      <NavigationContainer>
+      <MyTabs />
     </NavigationContainer>
+
   );
 
-  }
+}
+
+
+
 
